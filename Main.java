f86ce476a404945454941428d8561d7e94f18e74
AbhirajSinghRajpoot <Abhirajsingh2k5@gmail.com>
2026-03-13 21:10:53 +0530

    Refactor Main class to use try-with-resources for Scanner initialization

--- Main.java
+++ Main.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println("Enter the number of elements in the array: " + n);
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            System.out.println("Enter the number of elements in the array: " + n);
 
-        int[] arr = new int[n];
-        System.out.println("Enter the elements of the array:");
+            int[] arr = new int[n];
+            System.out.println("Enter the elements of the array:");
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
 
-        }
-        System.out.println("The elements of the array are:");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-            
+            }
+            System.out.println("The elements of the array are:");
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+
+            }
         }
 
     }
