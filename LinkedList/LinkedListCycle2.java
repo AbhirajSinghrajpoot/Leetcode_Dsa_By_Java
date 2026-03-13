f8d035a846556f86549300a9f741e48419804169
AbhirajSinghRajpoot <Abhirajsingh2k5@gmail.com>
2026-03-13 21:05:48 +0530

    Add LinkedListCycle2 to find cycle start node

---
+++ LinkedList/LinkedListCycle2.java
@@ -0,0 +1,59 @@
+public class LinkedListCycle2 {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static ListNode detectCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+
+                slow = head;
+
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+
+                return slow;
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode a = new ListNode(3);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(0);
+        ListNode d = new ListNode(-4);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = b;
+
+        ListNode result = detectCycle(a);
+
+        if (result != null) {
+            System.out.println(result.val);
+        } else {
+            System.out.println("No Cycle");
+        }
+    }
+}
