48278ff2f2b05b293ca26acc85e0a097053627e7
AbhirajSinghRajpoot <Abhirajsingh2k5@gmail.com>
2026-03-14 21:20:18 +0530

    Add reverse linked list solution

---
+++ LinkedList/ReverseLinkedList.java
@@ -0,0 +1,48 @@
+public class ReverseLinkedList {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
+        ListNode result = reverseList(a);
+
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
